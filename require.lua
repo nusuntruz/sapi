8b305ad12bd7629d1a7f1dfa071e640a791e4fdc
nusuntruz <75524995+nusuntruz@users.noreply.github.com>
2025-03-24 16:10:31 +0200

    Update require.lua

--- require.lua
+++ require.lua
@@ -4,9 +4,22 @@ cheat = require 'sapi\\sdk\\cheat'
 
 -- Low Level
 Utils = require 'sapi\\sdk\\utils'
---ProcBind = require 'sapi\\sdk\\processbind'
+--ProcBind = require 'sapi\\sdk\\processbind' -- // Sadly doesn t work on primordial, can make it work later
 Hooks = require 'sapi\\sdk\\hooks'
 vec2_t, vec3_t = table.unpack(require 'sapi\\sdk\\vectors')
 HSV, HEX, Color = table.unpack(require 'sapi\\sdk\\colors')
 Input = require 'sapi\\sdk\\input'
 ButtonCode_t, Buttons = Input.ButtonCode_t, Input.Buttons
+--Files = require 'sapi\\sdk\\filesystem' -- // Sadly doesn t work on skeet
+
+-- Game Calls / Api Library
+--panorama = require 'sapi\\sdk\\panorama' luv8 panorama
+Console = require 'sapi\\sdk\\convars'
+Netgraph = require 'sapi\\sdk\\netgraph'
+Client = require 'sapi\\sdk\\client'
+Globals = require 'sapi\\sdk\\globals'
+Engine = require 'sapi\\sdk\\engine'
+Render, EFontFlags, RoundingFlags = table.unpack(require 'sapi\\sdk\\render')
+Entity = require 'sapi\\sdk\\entity'
+Animate = require 'sapi\\sdk\\animate'
+json = require 'sapi\\sdk\\json'
